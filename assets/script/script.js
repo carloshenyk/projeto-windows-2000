1a0a13db28d6bb5ef65704ef753798565828120e
Carlos H L Santos <carloshenyk@gmail.com>
2023-04-13 23:19:54 -0300

    adicionando controle de volume do som

--- assets/script/script.js
+++ assets/script/script.js
@@ -33,3 +33,30 @@
 
   document.getElementById('relogio').innerHTML = horaformatada;
 },1000)
+
+// Abrir navegador
+
+function abrirNavegador(){
+  window.open("https://br.bing.com", "Nova Janela", "width=800,height=600");
+}
+
+// Volume
+
+function controleSom(){
+  const volume = document.querySelector('.areaDeTrabalho__volume');
+  if(volume.style.display === "none"){
+    volume.style.display = 'flex';
+  }else{
+    volume.style.display = 'none';
+  }
+}
+
+function rangeVolume(){
+  let resultado = document.querySelector('.rangeValor');
+  let volume = document.querySelector('.areaDeTrabalho__volume--controle');value;
+  
+  resultado.innerHTML = volume;
+}
+
+rangeVolume()
+document.addEventListener("change", rangeVolume);
